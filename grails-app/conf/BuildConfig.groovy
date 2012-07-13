58f1a771bf17aee2f18e22c95a94496f228341ae
David M. Carr <david@carrclan.us>
2012-07-13 09:48:30 -0400

    Add commented-out reference to mavenLocal, to enable easier testing of new versions

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -8,6 +8,7 @@ grails.project.dependency.resolution = {
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
         grailsCentral()
+        //mavenLocal()
     }
     dependencies {
     }
