87d08dbc1d3da77575a54acac2569af847719caf
David M. Carr <david@carrclan.us>
2012-07-13 10:02:40 -0400

    Add GitHub SCM location to metadata

--- PageResourcesGrailsPlugin.groovy
+++ PageResourcesGrailsPlugin.groovy
@@ -23,6 +23,7 @@ Enhances the resources plugin by allowing for creation of "page" resource module
 //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
 
     // Online location of the plugin's browseable source code.
+    def scm = [ url: "https://github.com/davidmc24/grails-page-resources/" ]
 //    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
 
     def doWithSpring = {
