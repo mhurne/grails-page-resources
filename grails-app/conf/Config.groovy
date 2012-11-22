aae1de3e25c31f981a434c42be2279532ce31e3a
David M. Carr <david@carrclan.us>
2012-11-21 20:14:24 -0500

    docs: revise copyright notice in docs to include 'and others'

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,4 +1,4 @@
 grails.doc.title = 'Page Resources Plugin'
 grails.doc.authors = 'David M. Carr'
 grails.doc.license = 'Apache License 2.0'
-grails.doc.copyright = 'Copyright 2012 David M. Carr'
+grails.doc.copyright = 'Copyright 2012 David M. Carr and others'
