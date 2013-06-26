f978a683dab5fb17fe2be1469d832c24f024bea9
David M. Carr <david@carrclan.us>
2013-06-25 21:27:24 -0400

    ci: attempt to add support for publishing to grails central

--- ci-build.sh
+++ ci-build.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env sh
-./grailsw package-plugin && ./grailsw doc
+mkdir ~/.grails/
+cat <<EOF > ~/.grails/settings.groovy
+grails.project.repos.grailsCentral.username='$GRAILS_USERNAME'
+grails.project.repos.grailsCentral.password='$GRAILS_PASSWORD'
+grails.project.portal.grailsCentral.username='$GRAILS_USERNAME'
+grails.project.portal.grailsCentral.password='$GRAILS_PASSWORD'
+EOF
+./grailsw package-plugin && ./grailsw doc && ./grailsw publish-plugin --non-interactive --no-scm --no-overwrite
