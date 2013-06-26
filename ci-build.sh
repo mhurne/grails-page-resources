bb045c84f935d7b4ab0ff362f5a0b192383584f4
David M. Carr <david@carrclan.us>
2013-06-25 22:07:12 -0400

    ci: allow publish-plugin to overwrite if snapshot

--- ci-build.sh
+++ ci-build.sh
@@ -1,9 +1,22 @@
-#!/usr/bin/env sh
-mkdir ~/.grails/
+#!/usr/bin/env bash
+set -e
+
+if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ];
+then
+mkdir -p ~/.grails/
 cat <<EOF > ~/.grails/settings.groovy
 grails.project.repos.grailsCentral.username='$GRAILS_USERNAME'
 grails.project.repos.grailsCentral.password='$GRAILS_PASSWORD'
 grails.project.portal.grailsCentral.username='$GRAILS_USERNAME'
 grails.project.portal.grailsCentral.password='$GRAILS_PASSWORD'
 EOF
-./grailsw package-plugin && ./grailsw doc && ./grailsw publish-plugin --non-interactive --no-scm --no-overwrite
+fi
+
+./grailsw package-plugin
+./grailsw doc
+if grep -q "version\s*=.*-SNAPSHOT" PageResourcesGrailsPlugin.groovy
+then
+./grailsw publish-plugin --non-interactive --no-scm --allow-overwrite
+else
+./grailsw publish-plugin --non-interactive --no-scm --no-overwrite
+fi
