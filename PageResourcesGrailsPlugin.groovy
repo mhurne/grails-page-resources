4e551bc67ba25c22336f5210c6a07b8ab8ffca61
David M. Carr <david@carrclan.us>
2014-02-26 23:35:30 -0500

    version: update to 0.2.4 for release

--- PageResourcesGrailsPlugin.groovy
+++ PageResourcesGrailsPlugin.groovy
@@ -23,7 +23,7 @@ import org.springframework.beans.factory.config.RuntimeBeanReference
 import org.springframework.core.io.FileSystemResource
 
 class PageResourcesGrailsPlugin {
-    def version = "0.2.4-SNAPSHOT"
+    def version = "0.2.4"
     def grailsVersion = "2.0 > *"
     def dependsOn = [resources: "1.2.6"]
     def loadAfter = ['webflow']
